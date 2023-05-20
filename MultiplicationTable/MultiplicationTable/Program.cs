17a6bbc70aaadb8b5d98ead1c67501f802c945b7
Syomai <126589688+Syomaii@users.noreply.github.com>
2023-05-20 17:47:14 +0800

    Add files via upload

---
+++ MultiplicationTable/MultiplicationTable/Program.cs
@@ -0,0 +1,30 @@
+﻿/*
+    Name: Christian Jay Putol
+    Date Created: 02/25/2023
+*/
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MultiplicationTable
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("\n\n\t\t       ---------------Multiplication Table-----------------\n");
+            for(int row = 1; row <= 10; row++) 
+            {
+                for (int col = 1; col <= 10; col++)
+                {
+                    int product = row * col;
+                    Console.Write("\t    "+product);
+                }
+                Console.WriteLine("\n");
+            }
+            Console.ReadLine();
+        }
+    }
+}
